ed2d94f544d688b319c3ac7c548f3844b8df595d
ARTHUR HORACIO ABREU <arthur_h_abreu@estudante.sesisenai.org.br>
2026-09-17 20:02:41 -0300

    byte e bun

---
+++ backend/server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const cardapio = [
+  {categoria: "pao", nome:"Frances", preco: 1.5},
+  {categoria: "pao", nome:"Integral", preco: 2.0},
+  {categoria: "pao", nome:"Ciabatta", preco: 2.5},
+  {categoria: "recheio", nome:"Frango", preco: 5.0},
+  {categoria: "recheio", nome:"Carne", preco: 6.5},
+  {categoria: "recheio", nome:"Vegetariano", preco: 4.0},
+  {categoria: "molho", nome:"Maionese", preco: 0.5},
+  {categoria: "molho", nome:"Mostarda", preco: 0.5},
+  {categoria: "molho", nome:"Especial", preco: 1.5},
+]
+
+function getPreco(categoria, nome) {
+    const item = cardapio.find(n => n.categoria === categoria && n.nome === nome)
+
+    return item.preco
+}
+
+app.use(cors())
+app.use(express.json());
+
+// ----- exercicio 01 ----- 
+app.get("/", (req, res) => {
+  res.send("Byte e Bun API no ar!")
+})
+
+// ------ exercicio 02 -----
+app.get("/cardapio", (req, res) => {
+  res.json(cardapio)
+})
+
+// ------ exercicio 03 -----// 
+app.get("/cardapio/:categoria", (req, res) => {
+  const categoria = req.params.categoria
+  const filtrados = cardapio.filter((item) => item.categoria === categoria);
+  res.json(filtrados);
+})
+
+// ------ exercicio 04 ------
+
+app.post("/pedido", (req, res) => {
+  const { pao, recheio, molho } = req.body
+  
+  const total = 
+  getPreco ("pao", pao) +
+  getPreco ("recheio", recheio) +
+  getPreco ("molho", molho);
+
+  res.json({
+    pao,
+    recheio,
+    molho,
+    total
+  })
+})
+
+
+app.listen(3000, () => {
+  console.log("Servidor Rodando em http://localhost:3000")
+});
+
+
---
+++ frontend/script.js
@@ -0,0 +1,46 @@
+
+async function carregarCardapio() {
+  const resposta = await fetch("http://localhost:3000/cardapio");
+  const dados = await resposta.json();
+  console.log(dados);
+}
+
+carregarCardapio();
+
+function popularSelect(idSelect, itens) {
+  const select = document.querySelector(`#${idSelect}`);
+
+  itens.forEach((item) => {
+    const option = document.createElement("option");
+    option.value = item.nome;
+    option.textContent = `${item.nome} — R$ ${item.preco}`;
+    select.appendChild(option);
+  });
+}
+
+botaoCalcular.addEventListener("click", async function () {
+  const pao = selectPao.value;
+  const recheio = selectRecheio.value;
+  const molho = selectMolho.value;
+
+  const resposta = await fetch("http://localhost:3000/pedido", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ pao, recheio, molho }),
+  });
+
+  const dados = await resposta.json();
+  document.querySelector("#resultado").textContent =
+        `Total do pedido: R$ ${dados.total}`;
+});
+
+
+async function carregarCardapio() {
+    const resposta = await fetch("http://localhost:3000/cardapio");
+    const dados = await resposta.json();
+
+    popularSelect("selectPao", dados.filter(item => item.categoria === "pao"));
+    popularSelect("selectRecheio", dados.filter(item => item.categoria === "recheio"));
+    popularSelect("selectMolho", dados.filter(item => item.categoria === "molho"));   
+}
+
