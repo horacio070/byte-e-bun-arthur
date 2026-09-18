3433a87bd47c44fcd1bc20c8e778fb117c9c3b37
Arthur <arthur_h_abreu@estudante.sesisenai.org.br>
2026-09-17 21:21:24 -0300

    corrige cors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const app = express();
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5500";
-app.use(cors({ origin: FRONTEND_URL }));
+const cors = require("cors");
 
+const app = express();
+
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5500";
+
+app.use(cors({ origin: FRONTEND_URL }))
 
 const cardapio = [
   {categoria: "pao", nome:"Frances", preco: 1.5},
